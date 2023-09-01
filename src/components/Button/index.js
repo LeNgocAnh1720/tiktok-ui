14c8c0a573b85daf7b04e39d7eae1aeb04b09cd3
ANHLN <anhln1720@gmail.com>
2023-09-01 15:03:54 +0700

    Design button all size

---
+++ src/components/Button/index.js
@@ -0,0 +1,52 @@
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({ to, href, primary = false, outline = false, text= false, small = false, large = false, disabled = false, rounded = false, children, className = false, leftIcon = false, rightIcon = false, onClick, ...passProps }) {
+    let Comp = 'button'
+
+    const props = {
+        onClick,
+        ...passProps
+    }
+
+    //Remove events listener when button is disabled
+    if(disabled) {
+        Object.keys(props).forEach((key) => {
+            if(key.startsWith('on') && typeof props[key] === 'function'){
+                delete props[key]
+            }
+        })
+    }
+
+    if(to){
+        props.to = to;
+        Comp = Link;
+    }else if(href){
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        small,
+        large,
+        disabled,
+        rounded,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,10 +5,10 @@ import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-s
 import Tippy from '@tippyjs/react/headless';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
-
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles)
 
@@ -58,7 +58,10 @@ function Header() {
                 </div>
             </Tippy>
 
-            <div className={cx('actions')}></div>
+            <div className={cx('actions')}>
+                <Button text>Upload</Button>
+                <Button primary> Log In </Button>
+            </div>
         </div>
     </header>
 }
